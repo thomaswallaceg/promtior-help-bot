0a3f542a50eb59e83dda2b12a556dcf26e7face7
Thomas Wallace <thomaswallaceg@gmail.com>
2025-03-02 16:05:37 -0300

    Initial Streamlit UI

---
+++ ui/ui.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage
+from langserve import RemoteRunnable
+
+bot = RemoteRunnable('http://localhost:8080')
+
+if 'message_history' not in st.session_state:
+    st.session_state.message_history = []
+
+def call_api(input: str) -> dict:
+    return bot.invoke({
+        "messages": st.session_state.message_history + [HumanMessage(input)],
+    })
+
+user_prompt=st.chat_input()
+
+if user_prompt is not None and user_prompt.strip() != "":
+    response = call_api(user_prompt)
+    new_messages = []
+    for msg in response["messages"]:
+        if  msg.type in ("human", "system") or (msg.type == "ai" and not msg.tool_calls):
+            new_messages.append(msg)
+    st.session_state.message_history = new_messages
+
+for msg in st.session_state.message_history:
+    with st.chat_message(msg.type, avatar=msg.type):
+        st.write(msg.content)
